96acb30ddc7e104bb95694575c5271966f7d235e
Paw <paw@pawrequest.com>
2025-10-03 12:43:32 +0100

    defauklt endpooint and accidentall quote reformat

--- src/parcelforce_expresslink/config.py
+++ src/parcelforce_expresslink/config.py
@@ -10,18 +10,20 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 def load_env():
-    ship_env = Path(os.getenv('PARCELFORCE_ENV'))
+    ship_env = Path(os.getenv("PARCELFORCE_ENV"))
     if not ship_env.exists():
-        raise ValueError('PARCELFORCE_ENV not set correctly')
-    logger.debug(f'Loading PARCELFORCE environment from {ship_env}')
+        raise ValueError("PARCELFORCE_ENV not set correctly")
+    logger.debug(f"Loading PARCELFORCE environment from {ship_env}")
     return ship_env
 
 
 def get_wsdl():
-    res = Path(cast(Path, files('parcelforce_expresslink').joinpath('expresslink_api.wsdl')))
+    res = Path(
+        cast(Path, files("parcelforce_expresslink").joinpath("expresslink_api.wsdl"))
+    )
     if not res.exists():
-        raise FileNotFoundError('WSDL file not found')
-    logger.info(f'Using WSDL file at {res}')
+        raise FileNotFoundError("WSDL file not found")
+    logger.info(f"Using WSDL file at {res}")
     return str(res.resolve())
 
 
@@ -31,17 +33,20 @@ class PFSettings(BaseSettings):
     """
 
     pf_ac_num_1: str
-    pf_ac_num_2: str | None
     pf_contract_num_1: str
-    pf_contract_num_2: str | None
-    department_id: int = 1
-
     pf_expr_usr: SecretStr
     pf_expr_pwd: SecretStr
-    pf_endpoint: str
+
+    department_id: int = 1
+    pf_ac_num_2: str | None
+    pf_contract_num_2: str | None
+
+    pf_endpoint: str = r"https://expresslink.parcelforce.net/ws"
     pf_wsdl: str = Field(default_factory=get_wsdl)
-    pf_binding: str = r'{http://www.parcelforce.net/ws/ship/v14}ShipServiceSoapBinding'
-    tracking_url_stem: str = r'https://www.royalmail.com/track-your-item#/tracking-results/'
+    pf_binding: str = r"{http://www.parcelforce.net/ws/ship/v14}ShipServiceSoapBinding"
+    tracking_url_stem: str = (
+        r"https://www.royalmail.com/track-your-item#/tracking-results/"
+    )
 
     model_config = SettingsConfigDict(env_ignore_empty=True, env_file=load_env())
 
