bd5b70a6af5cb00d3dabf21352b586fc5be8be32
Paw <paw@pawrequest.com>
2025-10-03 18:10:05 +0100

    test

--- tests/test_pf.py
+++ tests/test_pf.py
@@ -1,22 +1,33 @@
+import sys
 from datetime import date
 
+import parcelforce_expresslink.config
+import pytest
 from parcelforce_expresslink.client import ParcelforceClient
 from parcelforce_expresslink.address import Contact, AddressRecipient
 from parcelforce_expresslink.request_response import ShipmentResponse
 from parcelforce_expresslink.shipment import Shipment
 
 
+@pytest.fixture(autouse=True)
+def sandbox_only():
+    settings = parcelforce_expresslink.config.pf_settings()
+    if "test" not in settings.pf_endpoint:
+        pytest.skip("Skipping ParcelForce tests outside sandbox environment")
+        sys.exit()
+
+
 def test_readme():
     recip_address = AddressRecipient(
-        address_line1="An AddressLine",
-        town="A Town",
-        postcode="AA1BB2",
+        address_line1="Broadcasting House",
+        town="London",
+        postcode="W1A 1AA",
     )
     recip_contact = Contact(
         contact_name="A Name",
         email_address="anaddress@adomain.com",
         mobile_phone="07123456789",
-        business_name="A Business Name",
+        business_name="The BBC",
     )
     shipment = Shipment(
         recipient_address=recip_address,
